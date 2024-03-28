db1ab59c08f5d59a0c1c47c3ae2e88142005f83d
Sergey1995 <Serega19952@rambler.ru>
2024-03-28 13:07:59 +0300

    two commit

--- src/main/java/test/java/org/example/CalculatorTest.java
+++ src/main/java/test/java/org/example/CalculatorTest.java
@@ -1,10 +1,12 @@
+package test.java.org.example;
+
 import org.example.Calculator;
 import org.junit.Assert;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 public class CalculatorTest {
     @Test
-    public void add() {
+    public void testAdd() {
         Calculator calc = new Calculator();
         int result = calc.add(2, 4);
         Assert.assertEquals(6, result);
@@ -12,20 +14,20 @@ public class CalculatorTest {
 
     }
     @Test
-    public void sub() {
+    public void testSub() {
         Calculator calc = new Calculator();
         int result = calc.sub(10, 5);
         Assert.assertEquals(5, result);
     }
     @Test
-    public void mul() {
+    public void testMul() {
         Calculator calc = new Calculator();
         int result = calc.mul(20, 30) ;
         Assert.assertEquals(600, result);
       
     }
     @Test
-    public void div() {
+    public void testDiv() {
         Calculator calc = new Calculator();
         int result = calc.div(10,5) ;
         Assert.assertEquals(2, result);
